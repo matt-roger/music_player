c26389d8a896e8b9de7a01b3bfd92aed6a135022
zerotohero123 <39341756+zerotohero123@users.noreply.github.com>
2019-06-06 15:29:00 -0400

    Create WizSpeak.py

---
+++ WizSpeak.py
@@ -0,0 +1,27 @@
+def listen():
+	try:
+		r = sr.Recognizer()
+		mic = sr.Microphone()
+
+		print('Listening..........')
+		with mic as source:
+			r.adjust_for_ambient_noise(source)
+			audio = r.listen(source)
+		print('Understood')
+		query = r.recognize_google(audio)
+
+		print(query)
+		return query
+	except:
+
+		print("I dont understand.....")
+		time.sleep(.7)
+		os.system('cls')
+		return ""
+    
+def say(text):
+	sleepTime = len(text)/3
+	resp = gTTS(text = text, lang = 'en', slow = False)
+	resp.save('audio.mp3')
+	os.system('audio.mp3')
+	time.sleep(sleepTime)
