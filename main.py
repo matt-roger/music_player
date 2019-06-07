f8b74dd1dcf66522ccc2733b2d3bfaaf5325a110
zerotohero123 <39341756+zerotohero123@users.noreply.github.com>
2019-06-07 14:33:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def startup():
 	state = listen().upper()
 
 	while state != 'HEY WIZKID':
+		say('How can I help you')
 		startup()
 
 	main()
@@ -21,40 +22,50 @@ def main():
 	# new, top, or mix url
 	track_url = "https://soundcloud.com/search/sounds?q="
 	artist_url = "https://soundcloud.com/search/people?q="
+	joji_url = "https://soundcloud.com/boulon-roger/sets/joji"
+	cityPop_url = "https://soundcloud.com/boulon-roger/sets/city-pop"
 
 	# create the selenium browser
 	browser = webdriver.Chrome("chromedriver")
 
 	choice = str(listen())
-
-	choiceArr = choice.split()
-
-	#todo
 	
 
 	while choice == "":
 		choice = str(listen())
+		
+	choiceArr = choice.split().upper()
+	
+	for keyWord in choiceArr:
+		if keyWord == 'PLAYLIST':
+			for keyWord in choiceArr:
+				if keyWord == 'JOJI':
+					browser.get(joji_url)
+				elif keyWord == 'CITY':
+					browser.get(cityPop_url)
+		else:
+			
 
-	choice = "%20".join(choice.split())
-	print(choice)
+			choice = "%20".join(choice.split())
+			print(choice)
 
-	resp = urlopen(track_url + choice)
-	if resp.getcode() == 200:
-		soup = bs4.BeautifulSoup(resp.read(), "html")
+			resp = urlopen(track_url + choice)
+			if resp.getcode() == 200:
+				soup = bs4.BeautifulSoup(resp.read(), "html")
 
-		print('--------------------------------------------------')
-		print('---------------------------------------------------')
+				print('--------------------------------------------------')
+				print('---------------------------------------------------')
 
-		songs = soup.select("a[href]")[2:]
-		print(songs)
-		songLinks =[]
+				songs = soup.select("a[href]")[2:]
+				print(songs)
+				songLinks =[]
 
-		for index, song in enumerate(songs):
-			songLinks.append(song.get("href"))
+				for index, song in enumerate(songs):
+					songLinks.append(song.get("href"))
 
-		browser.get('https://soundcloud.com' + songLinks[4])
+				browser.get('https://soundcloud.com' + songLinks[4])
 
-		startup()
+	startup()
 
 
 if __name__ == '__main__':
